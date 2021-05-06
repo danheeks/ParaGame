3463cfaddc8d9f78a85efdbee05f609cf998d7db
danheeks <danheeks@gmail.com>
2021-05-06 16:32:03 +0100

    Almost achieving powered flight, but worried about the realism of the SlideJoint physics ( vibrations ). Various test parameters logged in the comments.

--- game.py
+++ game.py
@@ -16,7 +16,6 @@ pymunk.pygame_util.positive_y_is_up = True
 draw_options = pymunk.pygame_util.DrawOptions(screen)
 font_height = 30
 myfont = pygame.font.SysFont('Arial', font_height)
-
 image_pixel_scale = 63.0 # there are about 63 pixels per metre in the pictures I have
 image_scale = pixel_scale / image_pixel_scale
 
@@ -28,6 +27,7 @@ wingImg = pygame.transform.scale(wingImg, (int(wingImg.get_width()*image_scale),
 
 pilot_centre = Vec2d(49,64) * image_scale
 thrust = False
+thrust_on_wing = False
 wing_centre = Vec2d(93,56) * image_scale
 
 w = screen.get_width()
@@ -37,10 +37,74 @@ force_draw_factor = 0.003
 background_spacing = 10 # metres
 camera = None
 text_y = 0
-winch_length = 10.0 # used if machine type is winch
+winch_length = 10.0 # used if winch
+winch = False
+line_wave = 1.6
+line_wave_time = 0
 
-machine_type = 'winch'
-#machine_type = 'normal'
+# 240, -0.06, 0.5,  line_wave = 1.8 = 1030m :)
+# 240, -0.05, 0.5,  line_wave = 1.8 = 1056m :)
+# 240, -0.04, 0.5,  line_wave = 1.8 = over 2000m :)
+# 240, -0.03, 0.5,  line_wave = 1.8 = over 1900m :)
+# 240, -0.02, 0.5,  line_wave = 1.8    about 11m high after 1km
+# 240, 0.0, 0.5,  line_wave = 1.8   landed after 709m
+# 240, 0.04, 0.5,  line_wave = 1.8   landed after 387m
+
+
+# 280, -0.04, 0.5,  line_wave = 1.6  landed at 251m
+# 260, -0.04, 0.5,  line_wave = 1.6  landed at 617m
+# 240, -0.04, 0.5,  line_wave = 1.6  8.3m high after 1km
+# 220, -0.04, 0.5,  line_wave = 1.6  3.7m high after 1km
+# 200, -0.04, 0.5,  line_wave = 1.6  landed at 939m
+# 180, -0.04, 0.5,  line_wave = 1.6  landed at 600m ish
+
+
+# 240, -0.04, 0.4,  line_wave = 1.6  6.1m high after 1km
+# 240, -0.04, 0.45,  line_wave = 1.6 10.3m high after 1km
+# 240, -0.04, 0.5,  line_wave = 1.6  8.3m high after 1km
+# 240, -0.04, 0.55,  line_wave = 1.6 4.6m high after 1km
+
+
+# 240, -0.04, 0.5,  line_wave = 1.6 brake_angle = 0.05    landed after 830m
+# 240, -0.04, 0.5,  line_wave = 1.6 brake_angle = 0.00 
+# 240, -0.04, 0.5,  line_wave = 1.6 brake_angle = -0.05   landed after 315m
+
+
+# 240, -0.04, 0.7,  line_wave = 1.6  square angle wave    12.2m after 1km ( but another time landed after 661m )
+# 240, -0.04, 0.5,  line_wave = 1.6  square angle wave     0.2m after 1km
+# 240, -0.04, 0.6,  line_wave = 1.6  square angle wave     landed at 948m
+# 240, -0.04, 0.7,  line_wave = 1.6  sin angle wave        1.4m after 1km
+# 240, -0.05, 0.5,  line_wave = 1.6  square angle wave     landed at 565m
+# 240, -0.06, 0.5,  line_wave = 1.6  square angle wave     landed at 267m
+# 240, -0.03, 0.5,  line_wave = 1.6  square angle wave     12m at 1km
+
+
+# 240, -0.03, 0.5,  line_wave = 2.0  brake_angle 0.01 square angle wave  parachutal stall after about 100m
+# 240, -0.03, 0.5,  line_wave = 2.0  brake_angle -0.01 square angle wave   landed at 849m
+# 240, -0.03, 0.5,  line_wave = 2.0  brake_angle -0.02 square angle wave   landed at 460m
+
+# 240, -0.03, 0.5,  line_wave = 1.7  brake_angle 0.0 square angle wave  13.5m at 1km
+# 240, -0.035, 0.5,  line_wave = 1.7  brake_angle 0.0 square angle wave  3.5m at 1km
+
+
+# 240, -0.04, 0.5,  line_wave = 1.6 sin wave   943m
+# 240, -0.04, 0.3,  line_wave = 1.6 sin wave   2m at 1km
+# 240, -0.04, 0.15,  line_wave = 1.6 sin wave   3m at 1km
+# 240, -0.04, 0.05,  line_wave = 1.6 sin wave  459m
+
+
+# 230, -0.03, 0.05,  line_wave = 1.8 sin wave*6  9.6m at 1km
+
+
+line_wave_cycle_length = 240
+line_wave_angle_fraction = -0.04
+line_wave_angle_amplitude = 1.0
+line_wave_angle_offset = 0.5
+brake_angle = 0.03
+
+
+default_front_length = 6.3
+default_rear_length = 6.2
 
 class Graph:
     def __init__(self, pts):
@@ -75,11 +139,26 @@ class GameBody():
 
     def apply_parasitic_drag(self, coefficient):
         pass
+        
+    def draw_shape(self):
+        s = None
+        prev = None
+        for v in self.shape.get_vertices():
+            x,y = v.rotated(self.shape.body.angle) + self.shape.body.position
+            if s == None:
+                s = (x,y)
+            else:
+                p = (x,y)
+                if prev != None:
+                    draw_line(prev, p)
+                prev = (x,y)
+        if prev != None:
+            draw_line(prev, s)
 
 class Wing(GameBody):
-    def __init__(self):
+    def __init__(self, alt):
         poly_pts = [(0.635, -2.667),(-0.048, -2.556),(-0.841, -1.619),(-1.317, 0.698),(-0.984, 0.841),(1.349, 0.254),(1.016, -1.381)]
-        GameBody.__init__(self, (0,46),(-8,-0.8), 4, poly_pts)
+        GameBody.__init__(self, (0,alt),(-8,-0.8), 4, poly_pts)
         self.body.angle = 0.3
         self.airflow = Vec2d(0,0)
         self.wing_angle = Vec2d(0,0)
@@ -103,21 +182,6 @@ class Wing(GameBody):
     def get_pressure_pos(self, angle):
         return self.pressure_posns.get_y_at_x(angle)
         
-    def draw_shape(self):
-        s = None
-        prev = None
-        for v in self.shape.get_vertices():
-            x,y = v.rotated(self.shape.body.angle) + self.shape.body.position
-            if s == None:
-                s = (x,y)
-            else:
-                p = (x,y)
-                if prev != None:
-                    draw_line(prev, p)
-                prev = (x,y)
-        if prev != None:
-            draw_line(prev, s)
-        
     def draw(self):
         draw_image(wingImg, wing_centre, self.body)
         
@@ -133,8 +197,12 @@ class Wing(GameBody):
         
     def apply_force(self):
         self.angle_of_wing = self.body.angle - 3.1
+        if self.brake:    
+            self.angle_of_wing -= brake_angle
+        elif self.let_up:
+            self.angle_of_wing += brake_angle
+
         v, v_magn = self.body.velocity.normalized_and_length()
-        
         angle_of_airflow = math.atan2(v.y, v.x)
         self.angle_of_attack = (angle_of_airflow - self.angle_of_wing) * 57
         if self.angle_of_attack > 180: self.angle_of_attack -= 360
@@ -150,23 +218,6 @@ class Wing(GameBody):
         if drag > 2000:
             drag = 2000
             
-        if self.brake:
-            # increased lift and drag
-            lift *= 1.5
-            drag *= 1.6
-            if rear_line != None:
-                rear_line.max = 6.1
-        elif self.let_up:
-            # decreased lift and drag
-            lift *= 0.6
-            drag *= 0.8
-            if rear_line != None:
-                rear_line.max = 6.4
-        else:
-            if rear_line != None:
-                rear_line.max = 6.2
-            
-            
         self.lift = -v.perpendicular() * lift
         self.drag = -v * drag
         
@@ -174,11 +225,38 @@ class Wing(GameBody):
         
         world_pos = self.body.local_to_world((0,0))
         self.body.apply_force_at_world_point(self.lift + self.drag, world_pos)
+
+        if thrust_on_wing:
+            # 20 kg to the left
+            world_pos = self.body.local_to_world((0,0))
+            self.body.apply_force_at_world_point(Vec2d(-20 * 9.8, 0), world_pos)
+            
+        if line_wave > 0.0:
+            # up and down
+            global line_wave_time
+            global line_wave_cycle_length
+            cycle_angle = float(line_wave_time)/line_wave_cycle_length * 2 * math.pi
+            angle2 = cycle_angle + line_wave_angle_offset * math.pi
+            a = line_wave * math.sin(cycle_angle)
+            sin = math.sin(angle2) * 100
+            if sin > 1.0: sin = 1.0
+            elif sin < -1.0: sin = -1.0
+            b = line_wave * sin
+            
+            if rear_line != None:
+                rear_line.max = default_rear_length + a * line_wave_angle_amplitude - b * line_wave_angle_fraction
+                #damped_spring_rear.rest_length = rear_line.max
+            if front_line != None:
+                front_line.max = default_front_length + a * line_wave_angle_amplitude + b * line_wave_angle_fraction
+                #damped_spring_front.rest_length = front_line.max
+                line_wave_time += 1
+            if line_wave_time >= line_wave_cycle_length:
+                line_wave_time = 0
         
 class Centre(GameBody):
-    def __init__(self):
+    def __init__(self, alt):
         poly_pts = [(-0.3, -0.1), (-0.3, 0.1), (0.3, 0.1), (0.3, -0.1)]
-        GameBody.__init__(self, (0,40), (-8,-0.9), 10, poly_pts)
+        GameBody.__init__(self, (0,alt), (-8,-0.9), 3, poly_pts)
         
     def draw(self):
         draw_image(pilotImg, pilot_centre, self.body)
@@ -187,8 +265,20 @@ class Centre(GameBody):
         if thrust:
             # 20 kg to the left
             world_pos = self.body.local_to_world((0,0))
-            self.body.apply_force_at_world_point(Vec2d(-20 * 9.8, 0), world_pos)
+            #self.body.apply_force_at_world_point(Vec2d(-20 * 9.8, 0), world_pos)
         self.apply_parasitic_drag(0.1)
+        
+class Damper(GameBody):
+    def __init__(self, wing, pos_on_wing):
+        self.wing = wing
+        world_pos = wing.body.local_to_world(pos_on_wing)
+        poly_pts = [(-0.05, -0.05), (-0.05, 0.05), (0.05, 0.05), (0.05, -0.05)]
+        GameBody.__init__(self, world_pos + (0, -0.3), (-8,-0.9), 0.5, poly_pts)
+        self.damped_spring = pymunk.DampedSpring(wing.body, self.body, pos_on_wing, (0, 0), 0.3, 500, 0.3)
+        space.add(self.damped_spring)
+        
+    def draw(self):
+        draw_shape(self)
 
 class Pilot(GameBody):
     def __init__(self, starting_height):
@@ -212,13 +302,13 @@ class Pilot(GameBody):
         # winch
         if drop_line != None:
             if self.winch_up:
-                self.dline -= 0.001
-                if self.dline < -0.2:
-                    self.dline = -0.2
+                self.dline -= 0.002
+                if self.dline < -0.4:
+                    self.dline = -0.4
             else:
-                self.dline += 0.001
-                if self.dline > 0.2:
-                    self.dline = 0.2
+                self.dline += 0.002
+                if self.dline > 0.4:
+                    self.dline = 0.4
             drop_line.max += self.dline
             if drop_line.max < 0.5:
                 drop_line.max = 0.5
@@ -226,17 +316,6 @@ class Pilot(GameBody):
             if drop_line.max > winch_length:
                 drop_line.max = winch_length
                 self.dline = 0
-
-            if rear_line != None:
-                rear_line.max = 6.4 + self.dline * 2
-
-
-#            self.v_to_centre = get_rope_vector(drop_line)
-#            v, v_magn = self.v_to_centre.normalized_and_length()
-#            world_pos = self.body.local_to_world((0,0))
-#            self.body.apply_force_at_world_point(v  *force, world_pos)
-#            world_pos = centre.body.local_to_world((0,0))
-#            centre.body.apply_force_at_world_point(-v  *force, world_pos)
         
         self.apply_parasitic_drag(0.1)
             
@@ -283,15 +362,27 @@ def draw_background():
         
     if draw_ground:
         draw_rect(Vec2d(minx, miny), Vec2d(maxx, 0), (35,100,40))    
+        
+    if line_wave != 0.0:
+        if front_line != None:
+            line_h = front_line.max * 30
+            pygame.draw.rect(screen, (0,255,0), pygame.Rect(0.8 * w, 0.5*h - line_h, 10, line_h))
+        if rear_line != None:
+            line_h = rear_line.max * 30
+            pygame.draw.rect(screen, (255,0,0), pygame.Rect(0.8 * w + 20, 0.5*h - line_h, 10, line_h))
 
-    if wing.angle_of_attack != None: draw_text('Angle of attack = ' + '%.1f' % wing.angle_of_attack + ' degrees')
+    if wing.angle_of_attack != None:
+        draw_text('Angle of attack = ' + ('%.1f' % wing.angle_of_attack) + ' degrees')
+    
     draw_text('Height = ' + '%.1f' %(pilot.body.position.y) + 'm')
     draw_text('Thrust = ' + ('ON' if thrust else 'off'))
+    draw_text('Thrust on Wing = ' + ('ON' if thrust_on_wing else 'off'))
     draw_text('Airspeed = ' + '%.1f' % abs(wing.body.velocity) + 'm/s')
     draw_text('Distance = ' + '%.1f' % math.fabs(pilot.body.position.x) + 'm')
     draw_text('Winch = ' + ('ON' if pilot.winch_up else 'off'))
-    
+    draw_text('Line Wave = ' + '%.1f' % line_wave)
         
+       
 
 def add_wall(space, start, end):
     seg = pymunk.Segment(space.static_body, start, end, 0.05)
@@ -346,22 +437,32 @@ def update_camera_pos():
 add_wall(space, (-2000, 0), (2000, 0))
 
 
-wing = Wing()
 drop_line = None
-if machine_type == 'normal':
-    pilot = Pilot(40)
-    line_attacher = pilot
-else:
+if winch:
+    wing = Wing(46)
+    centre = Centre(40)
     pilot = Pilot(40-winch_length)
-    centre = Centre()
     line_attacher = centre
     drop_line = pymunk.SlideJoint(centre.body, pilot.body, (0, 0), (-0.1, 0.2), 0.05, winch_length)
     space.add(drop_line)
-        
-front_line = pymunk.SlideJoint(wing.body, line_attacher.body, (-1.3 , 0), (0.0, 0.2), 0.05, 6.3)
+else:
+    wing = Wing(26)
+    pilot = Pilot(20)
+    line_attacher = pilot
+ 
+#damper_front = Damper(wing, (-1.3, 0))
+#damper_rear = Damper(wing, (1.4, 0))
+
+front_line = pymunk.SlideJoint(wing.body, line_attacher.body, (-1.3, 0), (0.0, 0.2), 0.05, default_front_length)
 space.add(front_line)
-rear_line = pymunk.SlideJoint(wing.body, line_attacher.body, (1.4, 0), (0.0, 0.2), 0.05, 6.2)
+rear_line = pymunk.SlideJoint(wing.body, line_attacher.body, (1.4, 0), (0.0, 0.2), 0.05, default_rear_length)
 space.add(rear_line)
+
+#damped_spring_front = pymunk.DampedSpring(wing.body, line_attacher.body, (-1.3, 0), (0.0, 0.2), default_front_length, 5000, 0.3)
+#space.add(damped_spring_front)
+#damped_spring_rear = pymunk.DampedSpring(wing.body, line_attacher.body, (1.4, 0), (0.0, 0.2), default_rear_length, 5000, 0.3)
+#space.add(damped_spring_rear)
+
 
 
 while True:
@@ -387,6 +488,16 @@ while True:
                 pilot.winch_up = True
             if event.key == pygame.K_f:
                 wing.let_up = True
+            if event.key == pygame.K_g:
+                thrust_on_wing = not thrust_on_wing
+            if event.key == pygame.K_h:
+                if line_wave == 0.0:
+                    line_wave_time = 0.0
+                line_wave += 0.1
+            if event.key == pygame.K_i:
+                line_wave -= 0.1
+                if line_wave < 0.0:
+                    line_wave = 0.0
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_d:
                 wing.brake = False
